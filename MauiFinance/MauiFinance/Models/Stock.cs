9f99df856e31c48832d4a22368355b516f7d16ee
Thomas Crowe <wcrowe@g.clemson.edu>
2023-03-19 12:35:14 -0400

    Starting on models and services

---
+++ MauiFinance/MauiFinance/Models/Stock.cs
@@ -0,0 +1,22 @@
+﻿
+using MauiFinance.ViewModels;
+
+namespace MauiFinance.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public StockPrices stockPrices { get; set; }
+
+
+    }
+
+    public class StockDetail
+    {
+        
+        
+
+    }
+}
---
+++ MauiFinance/MauiFinance/Services/StockDataService.cs
@@ -0,0 +1,66 @@
+﻿using MauiFinance.Models;
+using System.Net.Http;
+using MauiFinance.Views;
+
+
+namespace MauiFinance.Services
+{
+    public class StockDataService
+    {
+        HttpClient _httpClient;
+        public StockDataService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+
+        //Service for search bar auto-complete
+        //searchTerm comes from search input in view
+        //public string searchTerm = "Microsoft";
+        //readonly string RAPIDAPIKEY = SecureStorage.
+        //public async Task<Stock> SearchAutoComplete(string searchTerm)
+        //{
+        //    var client = new HttpClient();
+        //    var request = new HttpRequestMessage
+        //    {
+        //        Method = HttpMethod.Get,
+        //        RequestUri = new Uri($"https://yh-finance.p.rapidapi.com/auto-complete?q={searchTerm}&region=US"),
+        //        Headers =
+        //            {
+        //                { "X-RapidAPI-Key", RAPIDAPIKEY },
+        //                { "X-RapidAPI-Host", "yh-finance.p.rapidapi.com" },
+        //            },
+        //    };
+        //    using (var response = await client.SendAsync(request))
+        //    {
+        //        response.EnsureSuccessStatusCode();
+        //        var body = await response.Content.ReadAsStringAsync();
+        //        Console.WriteLine(body);
+        //        //use body to fill Stock models and return it here to list in view
+        //    }
+        //}
+
+        //Service for getting specific stock data
+        //public async Task<StockPrices> GetStockData(string symbol)
+        //{
+        //    var client = new HttpClient();
+        //    var request = new HttpRequestMessage
+        //    {
+        //        Method = HttpMethod.Get,
+        //        RequestUri = new Uri($"https://yh-finance.p.rapidapi.com/stock/v2/get-chart?interval=1d&symbol={symbol}&region=US"),
+        //        Headers =
+        //            {
+        //                { "X-RapidAPI-Key", RAPIDAPIKEY },
+        //                { "X-RapidAPI-Host", "yh-finance.p.rapidapi.com" },
+        //            },
+        //    };
+        //    using (var response = await client.SendAsync(request))
+        //    {
+        //        response.EnsureSuccessStatusCode();
+        //        var body = await response.Content.ReadAsStringAsync();
+        //        Console.WriteLine(body);
+        //        //use body to fill StockPrices and return it to use in ChartViewModel/ChartPage
+
+
+    }
+}
