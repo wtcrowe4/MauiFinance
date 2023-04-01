4746861ce46608a068e3d6ccc00af19a2d924b1e
Thomas Crowe <wcrowe@g.clemson.edu>
2023-03-31 22:24:01 -0400

    Add service for best stocks for the day

--- MauiFinance/MauiFinance/Services/StockDataService.cs
+++ MauiFinance/MauiFinance/Services/StockDataService.cs
@@ -83,5 +83,29 @@ namespace MauiFinance.Services
         //        Console.WriteLine(body);
         //        //use body to fill StockDetail and return it to use in StockDetailViewModel/StockDetailPage
 
+        //Service for getting best stocks for the day
+        //public async Task<Stock> GetBestStocks()
+        //{
+        //    var client = new HttpClient();
+        //    var request = new HttpRequestMessage
+        //    {
+        //        Method = HttpMethod.Get,
+        //        RequestUri = new Uri("https://yh-finance.p.rapidapi.com/stock/v2/get-movers?region=US&lang=en-US"),
+        //        Headers =
+        //            {
+        //                { "X-RapidAPI-Key", RAPIDAPIKEY },
+        //                { "X-RapidAPI-Host", "yh-finance.p.rapidapi.com" },   
+        //            },
+        //    };
+        //    using (var response = await client.SendAsync(request))
+        //    {
+        //        response.EnsureSuccessStatusCode();
+        //        var body = await response.Content.ReadAsStringAsync();
+        //        Console.WriteLine(body);
+        //        //use body to fill Stock models and return it here to list in view
+        
+        
+
+
     }
 }
