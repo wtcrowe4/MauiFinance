745701fb17d1ac63a6ee86b9934cd7eeca01a6eb
Thomas Crowe <wcrowe@g.clemson.edu>
2023-04-06 08:28:24 -0400

    added watchlist

--- MauiFinance/MauiFinance/Services/StockDataService.cs
+++ MauiFinance/MauiFinance/Services/StockDataService.cs
@@ -104,7 +104,26 @@ namespace MauiFinance.Services
         //        Console.WriteLine(body);
         //        //use body to fill Stock models and return it here to list in view
         
-        
+        //Service for getting watchlist stocks
+        //public async Task<Stock> GetWatchlistStocks()
+        //{
+        //    var client = new HttpClient();
+        //    var request = new HttpRequestMessage
+        //    {
+        //        Method = HttpMethod.Get,
+        //        RequestUri = new Uri("https://yh-finance.p.rapidapi.com/stock/v2/get-movers?region=US&lang=en-US"),
+        //        Headers =
+        //            {
+        //                { "X-RapidAPI-Key", RAPIDAPIKEY },
+        //                { "X-RapidAPI-Host", "yh-finance.p.rapidapi.com" },
+        //            },
+        //    };
+        //    using (var response = await client.SendAsync(request))
+        //    {
+        //        response.EnsureSuccessStatusCode();
+        //        var body = await response.Content.ReadAsStringAsync();
+        //        Console.WriteLine(body);
+        //        
 
 
     }
